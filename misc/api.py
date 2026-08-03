b3e52559f0b7d5dda953acefd7c6741011ed537e
Duy <lengocduy169@gmail.com>
2026-08-03 23:07:18 +0700

    .

--- misc/api.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility export for SET-style Adversarial Perturbation Injection."""
-
-from ultralytics.nn.modules import AdversarialPerturbationInjection
-
-
-__all__ = ("AdversarialPerturbationInjection",)
